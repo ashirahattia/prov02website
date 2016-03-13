06f6653e0df88e7e18550317b3d54985835ff59a
Angira Shirahatti <ashirahattia@berkeley.edu>
2016-03-13 16:43:46 -0700

    adjusting port

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -19,4 +20,5 @@ def echo_loc(lat, lng):
     return ''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
